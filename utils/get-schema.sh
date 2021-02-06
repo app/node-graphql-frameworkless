61af6e5d3e9bcc30a74f7093063ff3115cb34e9b
Andrey Paskal <apaskal@gmail.com>
2021-02-06 16:03:07 +0300

    Retrive mutation types too along side with queries types

--- utils/get-schema.sh
+++ utils/get-schema.sh
@@ -5,6 +5,10 @@ FILE=introspect.gql
 LINE="$(cat $FILE | tr -d '\n')"
 QUERY=$(jq -n --arg q "$LINE" '{ query: $q }')
 
+echo "Queries"
 ## get Query types
-curl -s -H "Content-Type: application/json; charset=utf-8" -d "$QUERY" http://localhost:3080/graphql | jq '.data.__schema.types[]|select( .name=="Query").fields[0] '
+curl -s -H "Content-Type: application/json; charset=utf-8" -d "$QUERY" http://localhost:3080/graphql | jq '.data.__schema.types[]|select( .name=="Query").fields '
+echo Mutations
+## get Mutation types
+curl -s -H "Content-Type: application/json; charset=utf-8" -d "$QUERY" http://localhost:3080/graphql | jq '.data.__schema.types[]|select( .name=="Mutation").fields '
 
